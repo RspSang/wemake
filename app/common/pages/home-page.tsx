6f1ccac9bb990eda331a6cda57ca52f5d5ade0f4
Hyeonjun Park <akwek33@naver.com>
2025-03-24 23:29:08 +0900

    Product Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,10 +1,36 @@
-export default function HomePage({}) {
+import type { MetaFunction } from 'react-router';
+import { ProductCard } from '~/common/components/product-card';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Home | wemake' },
+    { name: 'description', content: 'Welcome to wemake' },
+  ];
+};
+
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">환영합니다!</h1>
-      <p className="mt-4 text-xl text-gray-600">
-        이곳에서 여러분의 여정이 시작됩니다.
-      </p>
-    </main>
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
