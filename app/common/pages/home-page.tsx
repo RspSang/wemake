ba6d2a4399f06cd52047e0714e1481e16c9122db
HealMon <akwek33@naver.com>
2026-01-20 10:31:06 +0900

    feat: 홈페이지 개선, Badge 컴포넌트 및 JobCard 컴포넌트 추가

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,6 +3,7 @@ import { ProductCard } from '~/features/products/components/product-card';
 import { PostCard } from '~/features/community/components/post-card';
 import { Button } from '../components/ui/button';
 import { IdeaCard } from '~/features/ideas/components/idea-card';
+import { JobCard } from '~/features/jobs/components/job-card';
 
 export const meta: MetaFunction = () => {
   return [
@@ -61,6 +62,7 @@ export default function HomePage() {
             avatarUrl="https://github.com/rspsang.png"
           />
         ))}
+      </div>
         <div className="grid grid-cols-3 gap-4">
           <div>
             <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -85,7 +87,33 @@ export default function HomePage() {
             />
           ))}
         </div>
-      </div>
+        <div className="grid grid-cols-4 gap-4">
+          <div>
+            <h2 className="text-5xl font-bold leading-tight tracking-tight">
+              Latest Jobs
+            </h2>
+            <p className="text-xl font-light text-foreground">
+              Find your dream job.
+            </p>
+            <Button variant="link" asChild className="text-lg p-0">
+              <Link to="/jobs">Explore all jobs &rarr;</Link>
+            </Button>
+          </div>
+          {Array.from({ length: 11 }).map((_, index) => (
+            <JobCard
+              key={`jobId-${index}`}
+              id={`jobId-${index}`}
+              company="Tesla"
+              companyLogoUrl="https://github.com/facebook.png"
+              companyHq="San Francisco, CA"
+              title="Software Engineer"
+              postedAt="12 hours ago"
+              type="Full-time"
+              positionLocation="Remote"
+              salary="$100,000 - $120,000"
+            />
+          ))}
+        </div>
     </div>
   );
 }
